ca73158090d2b4c334c240b4ce3292abff45ce62
Omar Faroque (Anik) <faroque.ae@gmail.com>
2020-05-17 19:17:06 -0500

    data types

---
+++ src/practice/data_types.rs
@@ -0,0 +1,26 @@
+fn main() {
+    let x:u32=10; // u means unsigned variable
+    println!("the value of x: {}", x);
+
+    let y:i32=-10; // u means signed variable
+    println!("the value of y: {}", y);
+
+    let f: f32 = 3.0;
+    println!("the value of f: {}", f); //floating point number
+
+    let sum = 5 + 10;
+    println!("{}",sum);
+    // subtraction
+    let difference = 95.5 - 4.3;
+    println!("{}",difference);
+
+    // multiplication
+    let product = 4 * 30;
+    println!("{}",product);
+    // division
+    let quotient = 56.7 / 32.2;
+    println!("{}",quotient);
+    // remainder
+    let remainder = 43 % 5;
+    println!("{}",remainder);
+}
