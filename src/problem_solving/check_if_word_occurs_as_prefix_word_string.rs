b7f1c0f080fa9d207f0dda83905e1773da651489
Omar Faroque (Anik) <faroque.ae@gmail.com>
2020-05-24 12:29:36 -0500

    code cleaning

--- src/problem_solving/check_if_word_occurs_as_prefix_word_string.rs
+++ src/problem_solving/check_if_word_occurs_as_prefix_word_string.rs
@@ -11,15 +11,12 @@ pub fn is_prefix_of_word(sentence: String, search_word: String) -> i32 {
     let count=-1;
     for i  in 0..str_vec.len() {
         let w = str_vec[i];
-
         if search_word.len()<=w.len()  {
             let slice = &w[..search_word.len()];
             if slice==search_word {
-                //println!("{}",count+1+);
                 return (i+1) as i32
             }
         }
     }
     return count;
-
 }
