a8686c949f41ae4585a540883ca648f4503cdacd
Omar Faroque (Anik) <faroque.ae@gmail.com>
2020-05-25 19:07:30 -0500

    async call

---
+++ src/practice/async_rust.rs
@@ -0,0 +1,36 @@
+use futures::executor::block_on;
+async fn learn_song()-> String{
+    println!("learning....");
+    return String::from("learning song");
+}
+async fn dance()-> String{
+    println!("dancing....");
+    return String::from("Dancing");
+}
+async fn sing_song(_song:String)-> String{
+    println!("singing....");
+    return String::from("singing song");
+}
+async fn learn_and_sing() {
+    // Wait until the song has been learned before singing it.
+    // We use `.await` here rather than `block_on` to prevent blocking the
+    // thread, which makes it possible to `dance` at the same time.
+    let song = learn_song().await;
+    sing_song(song).await;
+}
+
+async fn async_main() {
+    let f1 = learn_and_sing();
+    let f2 = dance();
+
+    // `join!` is like `.await` but can wait for multiple futures concurrently.
+    // If we're temporarily blocked in the `learn_and_sing` future, the `dance`
+    // future will take over the current thread. If `dance` becomes blocked,
+    // `learn_and_sing` can take back over. If both futures are blocked, then
+    // `async_main` is blocked and will yield to the executor.
+    futures::join!(f1, f2);
+}
+
+fn main() {
+    block_on(async_main());
+}
