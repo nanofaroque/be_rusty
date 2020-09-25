ea83bf00a33827de85048f9231a12ca5fdb8678d
Omar Faroque (Anik) <faroque.ae@gmail.com>
2020-09-25 14:38:54 -0500

    Update main.rs
    code cleaning

--- readcmdfromproc/src/main.rs
+++ readcmdfromproc/src/main.rs
@@ -16,16 +16,4 @@ fn main() {
     let mut contents = String::new();
     buf_reader.read_to_string(&mut contents).unwrap();    
     println!("{}",contents);
-
-//println!("Hello, world!");
-  //   let args: Vec<String> = env::args().collect();
-    
-    //let query = &args[1];
-   // let filename = &args[2];
-
-    //println!("Searching for {}", query);
-   // println!("In file {}", filename);
-
-
-
 }
