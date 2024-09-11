6488858c67d9dd04f22399e5bacd8ef9b7b3f0e4
Omar Faroque (Anik) <faroque.ae@gmail.com>
2024-09-10 22:51:44 -0500

    Update trait.rs

--- trait.rs
+++ trait.rs
@@ -1,1 +1,31 @@
+pub struct NewsArticle {
+  pub author: String,
+  pub headline: String,
+  pub content: String
+}
+
+impl Summary for NewsArticle {
+  fn summarize(&self) -> String {
+    format!("{}, by {}", self.headline, self.author);
+  }
+}
+
+pub struct Tweet {
+  pub username: String,
+  pub content: String,
+  pub reply: bool,
+  pub retweet: bool
+}
+impl Summary for Tweet {
+  fn summarize(&self) -> String {
+    format!("{}, by {}", self.content, self.username);
+  }
+}
+
+pub trait Summary{
+  fn summarize(&self) -> String;
+}
+
+
+
 
